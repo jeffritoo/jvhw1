0a5989750d22338d54bf948ffb55ba05c61099ed
jeffritoo <jeffryvdavid@gmail.com>
2021-05-31 14:44:45 -0700

    hw1

---
+++ App.js
@@ -0,0 +1,25 @@
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.text}>
+        Welcome to IS 657 from Jeffry Villatoro
+      </Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center"
+  },
+  text: {
+    fontWeight: "bold"
+  }
+});
+
